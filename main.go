b35431818efadd4c5df10f00f2d02a903c71fea0
RasoulZ <r.zamani72@gmail.com>
2025-05-03 09:11:13 +0330

    read tasks from json and notify user

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"time"
+)
+
+type Task struct {
+	Title    string    `json:"title"`
+	Message  string    `json:"message"`
+	DateTime time.Time `json:"datetime"`
+}
+
+func loadTasks(filename string) ([]Task, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var tasks []Task
+	err = json.Unmarshal(data, &tasks)
+	if err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
+}
+
+func notify(title, message string) {
+	cmd := exec.Command("notify-send", title, message)
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Failed to send notification:", err)
+	}
+}
+
+func main() {
+	tasks, err := loadTasks("tasks.json")
+	if err != nil {
+		fmt.Println("Error loading tasks:", err)
+		return
+	}
+
+	fmt.Println("📅 Task reminder started...")
+
+	for {
+		now := time.Now()
+		for i := range tasks {
+			task := &tasks[i]
+			if !task.DateTime.IsZero() && now.After(task.DateTime) && now.Sub(task.DateTime) < time.Second*2 {
+				notify(task.Title, task.Message)
+				task.DateTime = time.Time{}
+			}
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
